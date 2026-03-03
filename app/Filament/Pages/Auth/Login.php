9f3399e7746e1eb8f47f0cd8ef4f677c6669aa81
raearn <malig.raearn@gmail.com>
2026-03-04 02:01:43 +0800

    add toast notification

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -2,12 +2,30 @@
 
 namespace App\Filament\Pages\Auth;
 
+use Filament\Auth\Http\Responses\Contracts\LoginResponse;
 use Filament\Auth\Pages\Login as BaseLogin;
+use Filament\Notifications\Notification;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Validation\ValidationException;
 
 class Login extends BaseLogin
 {
     protected string $view = 'filament.pages.auth.login';
+
+    public function authenticate(): ?LoginResponse
+    {
+        try {
+            return parent::authenticate();
+        } catch (ValidationException $e) {
+            Notification::make()
+                ->title('Login Failed')
+                ->body($e->getMessage())
+                ->danger()
+                ->send();
+
+            return null;
+        }
+    }
 
     public function getHeading(): string | Htmlable | null
     {
